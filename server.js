895c4c568710adf16b8b94e4116c80f97312963f
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-03-17 13:22:31 +0400

    server fix

--- server.js
+++ server.js
@@ -39,11 +39,16 @@ app.use('/api/posts', posts);
 // Server static assets if in production
 if (port === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    // app.use(express.static('./client/build'));
+    app.use(express.static(path.join(__dirname, "client", "build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
+
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
