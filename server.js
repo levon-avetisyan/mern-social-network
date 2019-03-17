27033eb0427f3ffa4b590c402417b4cac332a176
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-03-17 12:36:37 +0400

    deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
@@ -19,27 +19,27 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db, {useNewUrlParser: true})
+    .connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-// Passport Middleware
+// Passport middleware
 app.use(passport.initialize());
 
-// Password Config
+// Passport Config
 require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
-app.use('/api/profiles', profile);
+app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-//Serve static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    //Set static folder
+    // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (res, req) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
